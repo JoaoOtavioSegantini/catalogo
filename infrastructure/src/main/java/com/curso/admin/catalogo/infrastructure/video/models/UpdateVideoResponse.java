9bb476476c411c4946490e437b6b793ce2f7e60c
João Otávio <joaotavioos@outlook.com>
2023-05-10 19:15:46 -0300

    feat: finish video aggregate ✨

---
+++ infrastructure/src/main/java/com/curso/admin/catalogo/infrastructure/video/models/UpdateVideoResponse.java
@@ -0,0 +1,6 @@
+package com.curso.admin.catalogo.infrastructure.video.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UpdateVideoResponse(@JsonProperty("id") String id) {
+}
